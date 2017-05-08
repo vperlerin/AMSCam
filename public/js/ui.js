777434a5b93e1fe53626e083db45bf3426474158
mikehankey <mikehankeyllc@gmail.com>
2017-05-08 15:47:49 +0000

    Ev management

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,8 +1,57 @@
+function test_one_selected($table) {
+   if($table.find('input[type=checkbox]:checked').length==0) {
+        $('button[data-action=delete-all]').prop('disabled',true);
+   } else {
+        $('button[data-action=delete-all]').prop('disabled',false);
+   } 
+}
+
 $(function() {
     
+    
+   // Load for screenshot 
    $('.load-on-click').click(function(e) {
        $(this).html('<span class="fa fa-spinner fa-spin"></span>');
        return true;
+   });
+    
+   // Select All events 
+   $('button[data-action=select_all]').click(function(e) {
+   	    var $table = $('#'+$(this).attr('data-table')), $checkbox;
+        
+       
+   	    if($($table.find('input[type=checkbox]').get(0)).is(':checked')) {
+				$table.find('input[type=checkbox]').each(function() {
+					var $t = $(this);
+					$t.prop('checked', false);
+					$t.closest($('tr')).removeClass('selected');
+				});
+   	    } else {
+				$table.find('input[type=checkbox]').each(function() {
+					var $t = $(this);
+					$t.prop('checked', true);
+					$t.closest($('tr')).addClass('selected');
+				});
+   	    }
+        
+        test_one_selected($table);
+ 			
+   }); 
+   
+   
+   // Select one event 
+   $('table#maybe-table').find('input[type=checkbox]').each(function() {
+        var $t = $(this);
+        $t.click(function() {
+            if($t.prop('checked')) {
+                $t.closest($('tr')).addClass('selected');
+            } else {
+                $t.closest($('tr')).removeClass('selected');
+            }
+            test_one_selected($('table#maybe-table')); 
+        });
+        
+         
    });
     
 });
