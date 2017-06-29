76691c8f18acd093aa1309ef3c78e3eef3c4f8fb
vperlerin <vperlerin@gmail.com>
2017-06-29 13:36:01 +0000

    Log Fix

--- routes/cam_log.js
+++ routes/cam_log.js
@@ -5,6 +5,7 @@ var router          = express.Router();
 var read_config     = require('../utils/read_config');
 var utils           = require('../utils/browser');
 var constants       = require('../utils/constants');
+var cookie       = require('../utils/cookie');
 
 /******************************************************************************************************************************************
 * Read Log
